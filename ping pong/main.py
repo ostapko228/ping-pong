a0f4a3211ba40c0d91c67aa17e68759d22e2df58
ostapko228 <104262860+ostapko228@users.noreply.github.com>
2022-04-23 16:51:20 +0300

    First pygame comite

--- ping pong/main.py
+++ ping pong/main.py
@@ -1,1 +1,20 @@
-print('hello world')
+
+
+back = (200, 255, 255)
+win_wight = 600
+win_height = 500
+window = display.set_mode((win_wight, win_height))
+window.fill(back)
+display.set_caption('Ping pong')
+
+game = True
+clock = time.Clock()
+FPS = 120
+
+while gsme:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+
+    display.update()
+    clock.tick(FPS)
