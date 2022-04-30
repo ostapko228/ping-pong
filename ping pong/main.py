7b0a33705fc41106e23f91bcaffb1ecd7fbb1f1e
ostapko228 <104262860+ostapko228@users.noreply.github.com>
2022-04-30 17:26:49 +0300

    comet

--- ping pong/main.py
+++ ping pong/main.py
@@ -14,17 +14,17 @@ class GameSprite(sprite.Sprite):
 class Player(GameSprite):
     def update_1(self):
         keys = key.get_pressed()
-        if keys[K_w] and self.self.rect.y < 5:
+        if keys[K_w] and self.rect.y > 5:
             self.rect.y -= self.speed
-        if keys[K_s]and self.self.rect.y < win_height - 120:
-            self.rect.y +- self.speed
+        if keys[K_s]and self.rect.y < win_height - 120:
+            self.rect.y += self.speed
 
     def update_r(self):
         keys = key.get_pressed()
-        if keys[K_UP] and self.self.rect.y < 5:
+        if keys[K_UP] and self.rect.y > 5:
             self.rect.y -= self.speed
-        if keys[K_DOWN]and self.self.rect.y < win_height - 120:
-            self.rect.y +- self.speed
+        if keys[K_DOWN]and self.rect.y < win_height - 120:
+            self.rect.y += self.speed
 
 back = (200, 255, 255)
 win_wight = 600
@@ -42,6 +42,18 @@ racket1 = Player('racket1.png', 30, 200, 4, 12, 120)
 racket2 = Player('racket2.png', 550, 200, 4, 12, 120)
 ball = GameSprite('ball.png', 200, 200, 4, 50, 50)
 
+speed_x = 3
+speed_y = 4
+
+score1 = 0
+score2 = 0
+
+font.init()
+font = font.Font(None, 35)
+scope = font.render('0 : 0', True, (0, 0, 0))
+lose = font.render('', True, (180, 0, 0))
+
+
 while game:
     for e in event.get():
         if e.type == QUIT:
@@ -51,8 +63,42 @@ while game:
         racket1.update_1()
         racket2.update_r()
 
+        if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2,ball):
+            speed_x *= -1
+            speed_y *= 1
+
+        if ball.rect.y > win_height-50 or ball.rect.y <0:
+            speed_y *= -1
+
+        if ball.rect.x <0:
+            score2 += 1
+            ball.rect.x = 200
+            ball.rect.y = 200
+            speed_x, speed_y =3, 4
+
+        if ball.rect.x > win_wight - 50:
+            score1 += 1
+            ball.rect.x, ball.rect.y = 200, 200
+            speed_x, speed_y =3, 4
+
+        score = font.render(f'{score1} : {score2}', True, (0, 0, 0))
+        window.blit(score, (270, 0))
+
         racket1.reset()
         racket2.reset()
+        ball.reset()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+
+        if score1 > 10:
+            lose = font.render('Player 2 lost!', True, (180, 0, 0))
+            finich = True
+            window.blit(lose, (250, 200))
+        if score2 > 10:
+            lose = font.render('Player 1 lost!', True, (180, 0, 0))
+            finich = True
+            window.blit(lose, (250, 200))
+
 
     display.update()
     clock.tick(FPS)
